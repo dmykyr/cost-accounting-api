bd03a7693419914c45b0e6590a4889aa1f49c97f
Dmytro Kyrychenko <dimakirimail@gmail.com>
2024-12-22 23:18:07 +0200

    enhancement: added automatic account creation on user creation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,11 +2,13 @@ import { User } from '../database/models/user';
 import { AddUserDto } from '../dtos/addUserDto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { AccountService } from '../accounts/account.service';
 
 export class UsersService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private readonly accountService: AccountService,
   ) {}
 
   async getAllUsers(): Promise<User[]> {
@@ -18,7 +20,9 @@ export class UsersService {
   }
 
   async addUser(dto: AddUserDto): Promise<User> {
-    return this.userRepository.save({ name: dto.name });
+    const user = await this.userRepository.save({ name: dto.name });
+    await this.accountService.addAccount({ userId: user.id, initialBalance: 0 });
+    return user;
   }
 
   async deleteUser(id: number) {
